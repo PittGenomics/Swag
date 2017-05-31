2bb2e6e58a1e97f077410379076bf233f6634c2f
Yadu Nand B <yadudoc1729@gmail.com>
2017-05-31 17:11:35 -0500

    Adding basic skeleton

--- SwiftSeq.py
+++ SwiftSeq.py
@@ -2,24 +2,41 @@
 
 import argparse
 import os
-
-import parsl
-from parsl import *
-
-workers = ThreadPoolExecutor(max_workers=8)
-dfk = DataFlowKernel(workers)
+import logging
 
 import bioapps as bio
+
+
+def readData(filename):
+   if os.path.exists(filename):
+      try:
+         with open(filename, 'r') as f:
+            return f.readlines()
+      except IOError as e:
+         logging.error("Caught IOError reading file:%s %s" % (filename, e))
+   else:
+      logging.error("File doesn't exist :%s" % filename)
+      
 
 
 if __name__ == "__main__":
 
    parser   = argparse.ArgumentParser()
    parser.add_argument("-v", "--verbose", default="DEBUG", help="set level of verbosity, DEBUG, INFO, WARN")
-   parser.add_argument("-l", "--logfile", default="task_executor.log", help="Logfile path. Defaults to ./task_executor.log")
-   parser.add_argument("-c", "--conffile", default="test.conf", help="Config file path. Defaults to ./test.conf")
-   parser.add_argument("-j", "--jobid", type=str, action='append')
-   parser.add_argument("-i", "--workload_id", default=None)
+   parser.add_argument("-l", "--logfile", default="swift_seq.log", help="Logfile")
+   parser.add_argument("-s", "--samples", default="analysis/individuals.txt", help="Individual samples")
    args   = parser.parse_args()
 
-   print(dir(bio))
+   logging.basicConfig(filename=args.logfile, level=logging.DEBUG,
+                       format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                       datefmt='%m-%d %H:%M')
+
+   x,_ = bio.Mosaik(f_inbam="1",
+                    f_readGroupStr="",
+                    sampleID="111",
+                    d_dir="./analysis",
+                    stdout='outputs/test.out',
+                    outputs=["outputs"])
+   x.result()
+   print(readData('outputs/test.out'))
+   

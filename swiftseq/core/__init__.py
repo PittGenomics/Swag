467869e9ebf68c35d916b9c878f1b89a07462323
Dominic Fitzgerald <djf604@truman.edu>
2017-12-11 15:13:02 -0600

    Added wrapper for filtering VCFs on a bed file

--- swiftseq/core/__init__.py
+++ swiftseq/core/__init__.py
@@ -286,6 +286,12 @@ class SwiftSeqApps(object):
             'exclusion': None,
             'walltime': '10:00:00',
                       'pool': 'primary'},
+        'FilterVcf': {
+            'name': 'FilterVcf',
+            'exclusion': None,
+            'walltime': '10:00:00',
+            'pool': 'primary'
+        },
         'RgMergeSort': {
             'name': 'RgMergeSort',
             'exclusion': None,
